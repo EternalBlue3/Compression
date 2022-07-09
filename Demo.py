21c8c667ff251b87c30f2bdc9abfa10e0b597192
Programmer Ammo <100178397+EternalBlue3@users.noreply.github.com>
2022-07-08 22:08:21 -0400

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -1,7 +1,9 @@
-from compression import *
+from Compression import *
 
 fh = open("File.txt","w")
 fh.write("0"*5000000)
 fh.close()
 
-gzip_compress(f"File.tar.gz",[f"File.txt"])
+gzip_compress("File.tar.gz",["File.txt"])
+
+_7z_compress("File.7z","File.txt"):
