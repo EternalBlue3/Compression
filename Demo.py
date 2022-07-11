59083e9ef5ee932b5c0d926e9a2a7ecde1762f77
Programmer Ammo <100178397+EternalBlue3@users.noreply.github.com>
2022-07-11 18:16:40 -0400

    Update Demo.py

--- Demo.py
+++ Demo.py
@@ -1,9 +1,15 @@
 from Compression import *
 
+file_write = "0" * 10000000, "1" * 10000000, "2" * 10000000, "3" * 10000000, "4" * 10000000, "5" * 10000000, "6" * 10000000, "7" * 10000000, "8" * 10000000, "9" * 10000000
+file_write = ''.join(file_write)
 fh = open("File.txt","w")
-fh.write("0"*5000000)
+fh.write(file_write)
 fh.close()
 
 compress_gzip("File.tar.gz",["File.txt"])
+
+compress_bzip2("File.bz2",["File.txt"])
+
+compress_lzma("File.xz",["File.txt"])
 
 compress_7z("File.7z","File.txt")
