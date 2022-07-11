35ee235fb1220154ec4b2b8dd3e48ccb41392bde
Programmer Ammo <100178397+EternalBlue3@users.noreply.github.com>
2022-07-11 18:15:15 -0400

    Update Compression.py

--- Compression.py
+++ Compression.py
@@ -2,6 +2,34 @@ import tarfile
 from tqdm import tqdm
 import py7zr
 
+def compress_lzma(file, members):
+    # open file for gzip compressed writing
+    tar = tarfile.open(file, mode="w:xz")
+    # with progress bar
+    # set the progress bar
+    progress = tqdm(members)
+    for member in progress:
+        # add file/folder/link to the tar file (compress)
+        tar.add(member)
+        # set the progress description of the progress bar
+        progress.set_description(f"Compressing {member}")
+    # close the file
+    tar.close()
+
+def compress_bzip2(file, members):
+    # open file for gzip compressed writing
+    tar = tarfile.open(file, mode="w:bz2")
+    # with progress bar
+    # set the progress bar
+    progress = tqdm(members)
+    for member in progress:
+        # add file/folder/link to the tar file (compress)
+        tar.add(member)
+        # set the progress description of the progress bar
+        progress.set_description(f"Compressing {member}")
+    # close the file
+    tar.close()
+
 def compress_gzip(tar_file, members):
     """
     Adds files (`members`) to a tar_file and compress it
